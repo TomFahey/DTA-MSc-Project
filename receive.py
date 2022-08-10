72b4b03de2fe5f387c4400de0b5298136f4994a7
Tom Fahey <tomp.fahey@gmail.com>
2022-08-10 08:59:19 +0100

    Changed details of connection teardown/start. Fixed bug with data_received function; added pickle serialisation of data objects, for transport over socket

--- receive.py
+++ receive.py
@@ -3,6 +3,9 @@ from socket import socket
 import serial_asyncio
 from functools import partial
 import numpy as np
+import sys
+import pickle as pkl
+np.set_printoptions(threshold=sys.maxsize)
 import ast     # Needed for parsing ascii messages to python objects
 
 class PicoDAServer(asyncio.Protocol):
@@ -20,18 +23,24 @@ class PicoDAServer(asyncio.Protocol):
         self.serial_reader = None
         self.serial_writer = None
         self.data = {'PID': np.array([0.]) ,'TEMP': np.array([0.]), 'TIME': np.array([0.])}
-        self.push_config = {'RUN': False, 'LOG': False, 'TARGET': 25}
-        self.pull_config = {'RUN': False, 'LOG': False, 'TARGET': 25}
+        self.push_config = {'RUN': False, 'MODE': False, 'LOG': False, 'TARGET': 25}
+        self.pull_config = {'RUN': False, 'MODE': False, 'LOG': False, 'TARGET': 25}
         self.device_status = {'PID': 0, 'TEMP': 0, 'TIME': 0}
+        self.background_task = None
 
     def connection_made(self, transport):
         self.transport = transport
         self.config['tcp_status'] = True
-        asyncio.ensure_future(self.setup_serial_connection())
+        print(self.config)
+        if not self.config['serial_status']:
+            asyncio.ensure_future(self.setup_serial_connection())
+        elif self.config['serial_status']:
+            self.background_task = self.loop.create_task(self.handle_serial_connection())
 
     def connection_lost(self, exc):
         self.config['tcp_status'] = False
-        self.exit_future.set_result(True)
+        self.background_task.cancel()
+
 
     async def setup_serial_connection(self):
         try:
@@ -44,21 +53,22 @@ class PicoDAServer(asyncio.Protocol):
                 self.serial_reader = serial_reader
                 self.serial_writer = serial_writer
                 print('Serial connection established')
-                task = self.loop.create_task(self.handle_serial_connection())
-                asyncio.ensure_future(task)
+                # Sync local config with device config
+                # (this has the side effect of resetting the device
+                # config to default settings if a TCP connection is
+                # lost and then re-established)
+                self.serial_writer.write(str({}).encode('utf-8'))
+                self.background_task = self.loop.create_task(self.handle_serial_connection())
         except serial_asyncio.serial.SerialException as e:
             print(e)
             self.config['serial_status'] = False
 
     async def handle_serial_connection(self):
-        print('Tried to serial_read')
         while self.config['tcp_status'] and self.config['serial_status']:
-            print(self.serial_reader.at_eof())
-            print(self.config['serial_status'])
             while not self.serial_reader.at_eof():
                 try:
                     msg = await self.serial_reader.readline()
-                    print(msg)
+                    #print(msg)
                     msg = msg.decode('utf-8').split('\n')[0]
                     if 'DATA:' in msg:
                         msg = msg.split('DATA:')[1]
@@ -75,7 +85,6 @@ class PicoDAServer(asyncio.Protocol):
                             if key in self.pull_config.keys():
                                 self.pull_config[key] = val
                     if self.pull_config != self.push_config:
-                        print('Statement triggered')
                         self.serial_writer.write(str(self.push_config).encode('utf-8'))
                         await self.serial_writer.drain()
                         #asyncio.ensure_future(self.serial_writer.drain())
@@ -92,34 +101,38 @@ class PicoDAServer(asyncio.Protocol):
 
     def write_data(self, data):
         self.transport.write(data)
+        print(data)
 
-    def data_received(self, data):
-        data = data.decode('utf-8').split('\n')[0]
-        if self.config['serial_status']:
-            if 'CONF:' in data:
-                data = data.split('CONF:')[1]
-                print(data)
-                data = ast.literal_eval(data)
-                for key,val in zip(data.keys(), data.values()):
-                    if key in self.push_config.keys():
-                        self.push_config[key] = val
-                    if key in self.config.keys():
-                        self.config[key] = val
-            elif 'QUERY:' in data:
-                data = data.split('QUERY:')[1]
-                data = ast.literal_eval(data)
-                response = {}
-                for key in data.keys():
-                    if key in self.device_status.keys():
-                        response[key] = self.device_status[key]
-            elif 'HISTORY:' in data:
-                data = data.split('HISTORY:')[1]
-                data = ast.literal_eval(data)
-                response = {}
-                for key in data.keys():
-                    if key in self.data.keys():
-                        response[key] = self.data[key]
-                self.write_data(str(response).encode('utf-8'))
+    def data_received(self, bin_msg):
+        msg = bin_msg.decode('utf-8').split('\n')
+        print(msg)
+        for line in msg[:-1]:
+            if self.config['serial_status']:
+                if 'CONFIG:' in line:
+                    line = line.split('CONFIG:')[1]
+                    data = ast.literal_eval(line)
+                    for key,val in zip(data.keys(), data.values()):
+                        if key in self.push_config.keys():
+                            self.push_config[key] = val
+                        if key in self.config.keys():
+                            self.config[key] = val
+                elif 'QUERY:' in line:
+                    line = line.split('QUERY:')[1]
+                    data = ast.literal_eval(line)
+                    response = {
+                        key:self.device_status[key] for key in data.keys()
+                            if key in self.device_status
+                        } if data.keys() else self.device_status
+                    self.write_data(str(response).encode('utf-8')+bytes('\n', 'utf-8'))
+                elif 'HISTORY:' in line:
+                    line = line.split('HISTORY:')[1]
+                    data = ast.literal_eval(line)
+                    response = {
+                            key:self.data[key].tolist() for key in data.keys() 
+                                               if key in self.data 
+                        } if data.keys() else  \
+                        {key:val.tolist() for key,val in self.data.items()}
+                    self.write_data(str(response).encode('utf-8')+b'\n')
 
 async def forward(reader, writer):
     try:
