9c91180684f1637bf159e154c818f9efafd700d2
Tom Fahey <tomp.fahey@gmail.com>
2022-08-10 08:41:33 +0100

    Simple utility script to assist with debugging/development

--- microcontroller/script.py
+++ microcontroller/script.py
@@ -10,25 +10,15 @@ import struct
 from ulab import numpy as np
 import countio
 
+from setup import *
 from PID import PIDState
 from sensor import TempSensor, TimeSensor, PIDSensor
 from log import Log
+from client import SerialClient
+from main import Supervisor
 
 
-
-# Create sensor object, communicating over the board's default SPI bus
-spi = busio.SPI(board.GP14, MISO=board.GP12, MOSI=board.GP11)
-cs = digitalio.DigitalInOut(board.GP10)  # Chip select of the MAX31865 board.
-
-max31865 = adafruit_max31865.MAX31865(spi, cs, rtd_nominal=1000.0, ref_resistor=4300.0, wires=4,baudrate=5_000_000)
-max31865.auto_convert = True
-max31865.bias = True
-
-print(max31865._read_u8(0x00))
-max31865._write_u8(0x00, 0xC0)
-print(max31865._read_u8(0x00))
-
-
+thisPID = PIDState(max31865, targetTemp, 2.5, 100., 1., 40.)
 
 tempSensor = TempSensor('TEMP', max31865)
 thisPIDState = PIDState(tempSensor, 23)
@@ -37,4 +27,7 @@ pidSensor = PIDSensor('PID', thisPIDState)
 
 thisLog = Log(0.25, [pidSensor, tempSensor, timeSensor])
 
+thisClient = SerialClient()
+
+thisSupervisor = Supervisor(thisClient, thisPID, thisLog)
 
