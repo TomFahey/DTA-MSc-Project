fcde8f6c6f2af33c944f3013ef7a6a2dd7dbf1cc
Tom Fahey <tomp.fahey@gmail.com>
2022-08-10 08:44:58 +0100

    Modified function names and added self.config, for better cohesion with other modules

--- microcontroller/client.py
+++ microcontroller/client.py
@@ -1,9 +1,11 @@
 import usb_cdc
+from utils import ResponsiveDict
 
 class SerialClient:
     
     def __init__(self, serial=usb_cdc.data, buf_out_size=1024,
                  timeout=0.01, write_timeout=0.015):
+        self.config = ResponsiveDict({})
         self.serial = serial
         self.serial.timeout = timeout
         self.serial.write_timeout = write_timeout
@@ -15,17 +17,18 @@ class SerialClient:
         
         self.buf_out_bytes_free = buf_out_size
 
-    def readSerial(self):
+    def read(self):
         readIn = self.serial.readline()
         return readIn
 
-    def writeSerial(self, data):
+    def write(self, data):
         return self.serial.write(data)
         
     def update(self):
         if self.serial.in_waiting > 0:
-                msg = self.readSerial()
+                msg = self.read()
                 if (len(msg) > 0 and msg.count(b'\n') < 1): # Timed out reading message
                     msg += self.serial.readline()
                 self.buf_in.append(msg)
-        self.buf_out_bytes_free += self.writeSerial(self.buf_out)
+        mv = memoryview(self.buf_out)
+        self.buf_out_bytes_free += self.write(mv[self.buf_out_bytes_free:])
